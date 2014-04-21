d27b890e539c560e89e00094266fb613b65e2f03
Severin Vaillancourt <severinvaillan@gmail.com>
2014-04-21 13:53:06 -0700

    deleted "tidy" data files, replaced with "sting_" .csv files

--- Stingray_Code_2014.R
+++
@@ -1,8 +0,0 @@
-setwd(dir="~/Documents/School//SP14/498//R_stingray_project_2014/")  
-STRmain <- read.csv(file= "tidy_sting_master.csv", header = TRUE)
-STRmain
-names(STRmain)
-str(STRmain)
-STRres <- read.csv(file= "tidy_sting_residency.csv")
-STRres
-str(STRres)
