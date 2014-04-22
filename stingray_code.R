8309ee4d0a7ea26ec7c4e7da61d5061824e6f514
Severin Vaillancourt <severinvaillan@gmail.com>
2014-04-22 01:23:42 -0700

    tinkering with subselections and boxplots

--- stingray_code.R
+++ stingray_code.R
@@ -1,3 +1,6 @@
 SRmain <- read.csv(file = "sting_master.csv", header = TRUE)
 SRres <- read.csv(file = "sting_residency.csv", header = TRUE)
 SRioa <- read.csv(file = "sting_ioa.csv", header = TRUE)
+SRmain1m <- SRmain[SRmain$Survey == 1 & SRmain$Sex == "m" , ]
+SRmain1m
+M1 <- boxplot(TL ~ Sex, data = SRmain)
