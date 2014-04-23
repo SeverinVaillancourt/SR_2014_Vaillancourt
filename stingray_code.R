6ffa277f47a9562afc05413eb1651d4e6247d084
Severin Vaillancourt <severinvaillan@gmail.com>
2014-04-22 23:18:42 -0700

    new quick excel file for correlations, updated .csv files and experimenting with code

--- stingray_code.R
+++ stingray_code.R
@@ -1,6 +1,28 @@
 SRmain <- read.csv(file = "sting_master.csv", header = TRUE)
 SRres <- read.csv(file = "sting_residency.csv", header = TRUE)
 SRioa <- read.csv(file = "sting_ioa.csv", header = TRUE)
+SRquick <- read.csv(file = "Quick_Sting_master_n16_2014.csv", header = TRUE)
+SRmain$fSurvey <- factor(SRmain$Survey)
+SRmain$fID <- factor(SRmain$ID)
+
+
+#test 
 SRmain1m <- SRmain[SRmain$Survey == 1 & SRmain$Sex == "m" , ]
-SRmain1m
-M1 <- boxplot(TL ~ Sex, data = SRmain)
+M1 <- boxplot(HAS ~ Treatment, data = SRmain)
+#test boxplot
+boxplot(TL ~ fSurvey * Sex, data = SRmain)
+#just females from SRmain
+SRmFe <- subset(SRmain, SRmain$Sex == "f")
+SRmFe
+#just males from SRmain
+SRmMa <- subset(SRmain, SRmain$Sex == "m")
+SRmMa
+library("plyr")
+
+reg1 <- lm(SRmain$TL ~ SRmain$IndResscore)
+reg1
+par(cex = .8)
+plot(SRmain$Area, SRmain$IndResscore)
+abline(reg1)
+
+hist(SRres$HAS)asf
